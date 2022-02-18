356e395072b5ae08814b1ef61da855aa7872b85c
ido neamani <ido.neamani@gmail.com>
2022-02-18 16:08:31 +0200

    fix all sql injection in lite file from studio

--- WebGoat/App_Code/DB/SqliteDbProvider.cs
+++ WebGoat/App_Code/DB/SqliteDbProvider.cs
@@ -187,8 +187,11 @@ namespace OWASP.WebGoat.NET.App_Code.DB
                 {
                     connection.Open();
 
-                    string sql = "select email from CustomerLogin where customerNumber = " + customerNumber;
+                    //string sql = "select email from CustomerLogin where customerNumber = " + customerNumber;
+                    string sql = "select email from CustomerLogin where customerNumber = @customerNumber";
+
                     SqliteCommand command = new SqliteCommand(sql, connection);
+                    command.Parameters.AddWithValue("@customerNumber",customerNumber)
                     output = command.ExecuteScalar().ToString();
                 } 
             }
@@ -557,8 +560,9 @@ namespace OWASP.WebGoat.NET.App_Code.DB
                 {
                     connection.Open();
 
-                    string sql = "select email from CustomerLogin where customerNumber = " + num;
+                    string sql = "select email from CustomerLogin where customerNumber = @num";
                     SqliteCommand cmd = new SqliteCommand(sql, connection);
+                    command.Parameters.AddWithValue("@num",num)
                     output = (string)cmd.ExecuteScalar();
                 }
                 
